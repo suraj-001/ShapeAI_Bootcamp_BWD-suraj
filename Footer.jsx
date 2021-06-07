994443b2e3d581bcac774385c033e4051744395e
suraj-001 <75194479+suraj-001@users.noreply.github.com>
2021-06-07 08:21:55 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+
+function Footer(){
+
+    var currYear=new Date().getFullYear();
+
+    return(
+        <footer>
+            <p>Copyright @ {currYear}</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import react from 'react';
+
+function Note(){
+    return(
+        <div className="note">
+         <h2>Javascript and React.js</h2>
+         <p>This was an amazing bootcamp by Shaurya Sir.</p>
+         We covered everything in this from scratch including javascript, html and Reactjs.
+        </div>
+    );
+}
+
+export default Note;
